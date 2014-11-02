eccc552d41be6c7e60c26d4a9d8daa3deaccc72c
mouse-pallet <tea_erra@yahoo.co.jp>
2014-11-03 00:02:31 +0900

    SentenceUnderstanding.jsを変更しました

--- SentenceUnderstanding.js
+++ SentenceUnderstanding.js
@@ -1,27 +1,26 @@
-
 // バイナリ化した画像をPOSTで送る関数
 var sendImageBinary = function(blob) {
     console.log("succcess2");
     var formData = new FormData();
     formData.append('image', blob);
     $.ajax({
-	    type: 'POST',
-		url: 'https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/scene?APIKEY=31723932483856747674576265745258735441562e653057316e65575a733465314f596c4b447263456a35',
-		data: formData,
+   type: 'POST',
+url: 'https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/scene?APIKEY=31723932483856747674576265745258735441562e653057316e65575a733465314f596c4b447263456a35',
+data: formData,
                 headers: {'Content-Type':'multipart/form-data'},
-		processData: false,
-		success:function(date, dataType){
-		console.log("succcess");
-		console.log(data);
-		//var $img = $('img');
-		//var imgSrc = $img.attr('src');
-		//$img.attr('src', "");
-		//$img.attr('src', imgSrc);
-	    },
-		error: function(XMLHttpRequest, textStatus, errorThrown){
-		console.log("error");
-	    }
-	});
+processData: false,
+success:function(date, dataType){
+console.log("succcess");
+console.log(data);
+//var $img = $('img');
+//var imgSrc = $img.attr('src');
+//$img.attr('src', "");
+//$img.attr('src', imgSrc);
+   },
+error: function(XMLHttpRequest, textStatus, errorThrown){
+console.log("error");
+   }
+});
 };
 // 読み込み成功時のコールバック関数
 var handleLoadSuccess = function(e){
@@ -33,8 +32,6 @@ var handleLoadSuccess = function(e){
 
 console.log("succcessStart");
 // 画像読み込み時の各イベントリスナ
-reader= new FileReader();
-reader.reader.readasbinarystring("");
 img=new ImageData();
 img.addEventListener('load', handleLoadSuccess);
 img.src = '/Users/Yuko/Desktop/mojininshiki_image.jpg';// 読み込み画像のパス
